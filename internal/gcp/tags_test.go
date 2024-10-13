ef81f6c4b0647e5b71b357382370d068fa7ab1d9
jijinp <jijin.puthuvakkattil@deliveryhero.com>
2024-10-13 13:34:44 +0200

    gcp tags tests

--- internal/gcp/tags_test.go
+++ internal/gcp/tags_test.go
@@ -1,88 +1,22 @@
 package gcp
 
 import (
-	"context"
 	"testing"
 
-	resourcemanagerpb "cloud.google.com/go/resourcemanager/apiv3/resourcemanagerpb"
-	"github.com/deliveryhero/gcp-config-connector-tagging-operator/internal/gcp/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTagsManager_CreateKey(t *testing.T) {
-
-	mockTagsManager := mocks.NewTagsManager(t)
-
-	ctx := context.Background()
-	projectID := "test-project"
+func TestCacheKeyTagKey(t *testing.T) {
 	key := "test-key"
-	expectedTagKey := &resourcemanagerpb.TagKey{Name: "projects/test-project/keys/test-key"}
-
-	mockTagsManager.On("CreateKey", ctx, projectID, key).Return(expectedTagKey, nil)
-
-	result, err := mockTagsManager.CreateKey(ctx, projectID, key)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedTagKey, result)
-
-	mockTagsManager.AssertExpectations(t)
+	expected := "key:test-key"
+	actual := cacheKeyTagKey(key)
+	assert.Equal(t, expected, actual)
 }
 
-func TestTagsManager_CreateValue(t *testing.T) {
-
-	mockTagsManager := mocks.NewTagsManager(t)
-
-	ctx := context.Background()
-	projectID := "test-project"
+func TestCacheKeyTagValue(t *testing.T) {
 	key := "test-key"
 	value := "test-value"
-	expectedTagValue := &resourcemanagerpb.TagValue{Name: "projects/test-project/keys/test-key/values/test-value"}
-
-	mockTagsManager.On("CreateValue", ctx, projectID, key, value).Return(expectedTagValue, nil)
-
-	result, err := mockTagsManager.CreateValue(ctx, projectID, key, value)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedTagValue, result)
-
-	mockTagsManager.AssertExpectations(t)
-}
-
-func TestTagsManager_LookupKey(t *testing.T) {
-
-	mockTagsManager := mocks.NewTagsManager(t)
-
-	ctx := context.Background()
-	projectID := "test-project"
-	key := "test-key"
-	expectedTagKey := &resourcemanagerpb.TagKey{Name: "projects/test-project/keys/test-key"}
-
-	mockTagsManager.On("LookupKey", ctx, projectID, key).Return(expectedTagKey, nil)
-
-	result, err := mockTagsManager.LookupKey(ctx, projectID, key)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedTagKey, result)
-
-	mockTagsManager.AssertExpectations(t)
-}
-
-func TestTagsManager_LookupValue(t *testing.T) {
-
-	mockTagsManager := mocks.NewTagsManager(t)
-
-	ctx := context.Background()
-	projectID := "test-project"
-	key := "test-key"
-	value := "test-value"
-	expectedTagValue := &resourcemanagerpb.TagValue{Name: "projects/test-project/keys/test-key/values/test-value"}
-
-	mockTagsManager.On("LookupValue", ctx, projectID, key, value).Return(expectedTagValue, nil)
-
-	result, err := mockTagsManager.LookupValue(ctx, projectID, key, value)
-
-	assert.NoError(t, err)
-	assert.Equal(t, expectedTagValue, result)
-
-	mockTagsManager.AssertExpectations(t)
+	expected := "value:test-key:test-value"
+	actual := cacheKeyTagValue(key, value)
+	assert.Equal(t, expected, actual)
 }
