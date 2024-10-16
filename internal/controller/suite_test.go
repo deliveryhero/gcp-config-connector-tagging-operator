03470379f01bce88c0e336d337c48646bce36ef2
jijinp <jijin.puthuvakkattil@deliveryhero.com>
2024-10-16 23:49:17 +0200

    fix: reset suite tests

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -17,129 +17,69 @@ limitations under the License.
 package controller
 
 import (
-	"context"
 	"fmt"
-	"os"
 	"path/filepath"
-	osruntime "runtime"
+	"runtime"
 	"testing"
-
-	resourcemanager "cloud.google.com/go/resourcemanager/apiv3"
-	ctrl "sigs.k8s.io/controller-runtime"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/stretchr/testify/mock"
 
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
-	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	storagev1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients/generated/apis/storage/v1beta1"
-	tagsv1alpha1 "github.com/GoogleCloudPlatform/k8s-config-connector/pkg/clients/generated/apis/tags/v1alpha1"
-	"github.com/deliveryhero/gcp-config-connector-tagging-operator/internal/gcp"
-	"github.com/deliveryhero/gcp-config-connector-tagging-operator/internal/util"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	// +kubebuilder:scaffold:imports
 )
 
-var (
-	cfg       *rest.Config
-	k8sClient client.Client
-	testEnv   *envtest.Environment
-	ctx       context.Context
-	cancel    context.CancelFunc
-)
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
 
 func TestControllers(t *testing.T) {
 	RegisterFailHandler(Fail)
+
 	RunSpecs(t, "Controller Suite")
 }
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
-	ctx, cancel = context.WithCancel(context.TODO())
-
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config")},
-		ErrorIfCRDPathMissing: true,
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: false,
+
+		// The BinaryAssetsDirectory is only required if you want to run the tests directly
+		// without call the makefile target test. If not informed it will look for the
+		// default path defined in controller-runtime which is /usr/local/kubebuilder/.
+		// Note that you must have the required binaries setup under the bin directory to perform
+		// the tests directly. When we run make test it will be setup and used automatically.
 		BinaryAssetsDirectory: filepath.Join("..", "..", "bin", "k8s",
-			fmt.Sprintf("1.30.0-%s-%s", osruntime.GOOS, osruntime.GOARCH)),
+			fmt.Sprintf("1.30.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
 	}
 
 	var err error
+	// cfg is defined in this file globally.
 	cfg, err = testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
 	// +kubebuilder:scaffold:scheme
 
-	scheme := runtime.NewScheme()
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-	utilruntime.Must(tagsv1alpha1.AddToScheme(scheme))
-	utilruntime.Must(storagev1beta1.AddToScheme(scheme))
-
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
-	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{Scheme: scheme})
-	Expect(err).ToNot(HaveOccurred())
-
-	targetLabels := ".*"
-	labelMatcher, _ := util.LimitLabelsWithRegex(targetLabels)
-
-	ctx := context.Background()
-	tagKeysClient, err := resourcemanager.NewTagKeysClient(ctx)
-	if err != nil {
-		setupLog.Error(err, "unable to create tag keys client")
-		os.Exit(1)
-	}
-	tagValuesClient, err := resourcemanager.NewTagValuesClient(ctx)
-	if err != nil {
-		setupLog.Error(err, "unable to create tag values client")
-		os.Exit(1)
-	}
-	tagsManager := gcp.NewTagsManager(tagKeysClient, tagValuesClient)
-
-	CreateTaggableResourceController(k8sManager, tagsManager, &StorageBucketMetadataProvider{}, labelMatcher)
-
-	go func() {
-		defer GinkgoRecover()
-		err = k8sManager.Start(ctx)
-		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
-	}()
 })
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
-	cancel()
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
 })
-
-var _ ResourceMetadataProvider[storagev1beta1.StorageBucket] = &StorageBucketMetadataProvider{}
-
-type StorageBucketMetadataProvider struct{}
-
-func (in *StorageBucketMetadataProvider) GetResourceLocation(r *storagev1beta1.StorageBucket) string {
-	return ptr.Deref(r.Spec.Location, "")
-}
-
-func (in *StorageBucketMetadataProvider) GetResourceID(_ string, r *storagev1beta1.StorageBucket) string {
-	name := r.Name
-	if r.Spec.ResourceID != nil {
-		name = *r.Spec.ResourceID
-	}
-	return fmt.Sprintf("//storage.googleapis.com/projects/_/buckets/%s", name)
-}
-
-type TagsManager struct {
-	mock.Mock
-}
